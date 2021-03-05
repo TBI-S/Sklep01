7abb1374266f850ee645f2b5f0d57e31d3383e65
TBI-S <rafattjaroma@gmail.com>
2021-03-05 10:09:34 +0100

    project commit 01

--- Produkty/views.py
+++ Produkty/views.py
@@ -14,20 +14,26 @@ def index(request):
     dane = {'kategorie' : kategorie}
     return render(request, 'szablon.html', dane)
 
-'''
+
 
 def kategoria(request, id):
     kategoria_user = Kategoria.objects.get(pk=id)
-    return HttpResponse(kategoria_user.nazwa)
+    kategoria_produkt = Produkty.objects.filter(kategoria = kategoria_user)
+    kategorie = Kategoria.objects.all()
+    dane = {
+        'kategoria_user' : kategoria_user,
+        'kategoria_produkt' : kategoria_produkt,
+        'kategorie' : kategorie
+    }
+    return render(request, 'kategoria_produkt.html', dane)
 
 def produkt(request, id):
     produkt_user = Produkty.objects.get(pk=id)
-    napis = "<h1>" + str(produkt_user) + "</h1>" + \
-            "<p>" + str(produkt_user.opis) + "</p>" + \
-            "<p>" + str(produkt_user.cena) + "/<p>"
-    return HttpResponse(napis)
+    kategorie = Kategoria.objects.all()
+    dane = {'produkt_user' : produkt_user, 'kategorie' : kategorie }
+    return render(request, 'produkt.html', dane)
 
-'''
+
 
 
 
--- sklep/urls.py
+++ sklep/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    # path('kategoria/<id>/', kategoria, name='kategoria'),
-    # path('produkt/<id>/', produkt, name='produkt'),
+    path('kategoria/<id>/', kategoria, name='kategoria'),
+    path('produkt/<id>/', produkt, name='produkt'),
 ]
